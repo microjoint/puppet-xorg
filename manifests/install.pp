3258b1ec601a48d612b70337ac0b452801c819fa
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2014-08-06 18:11:47 +0200

    missing install file<

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,7 @@ class xorg::install inherits xorg {
 
   package { 'xorg-server':
     ensure  => $package_ensure,
-    require => Package['video-card'],
+   # require => Package['video-card'],
   }
   package { 'xorg-xinit':
     ensure  => $package_ensure,
